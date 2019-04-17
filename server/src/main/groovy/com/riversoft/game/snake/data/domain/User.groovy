edf6c4f5b698f8493bf743d85c22ed21230a7767
shvets_y <shvets@river-soft.net>
2019-04-17 14:05:56 +0300

    монго в докере

--- server/src/main/groovy/com/riversoft/game/snake/data/domain/User.groovy
+++ server/src/main/groovy/com/riversoft/game/snake/data/domain/User.groovy
@@ -1,7 +1,6 @@
 package com.riversoft.game.snake.data.domain
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = 'users')
@@ -10,7 +9,6 @@ class User {
     @Id
     String id
 
-    @Indexed(unique = true)
     String username
 
     String passwordHash
