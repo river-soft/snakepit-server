c6fd316319a8976a9867410e0f7e85d08ad91edf
shvets_y <shvets_y@TeamCity>
2019-05-15 07:19:41 +0000

    TeamCity change in 'snakepit' project: VCS roots of 'Build front' build configuration were updated

--- .teamcity/patches/buildTypes/BuildFront.kts
+++ .teamcity/patches/buildTypes/BuildFront.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("BuildFront")
     name = "Build front"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
